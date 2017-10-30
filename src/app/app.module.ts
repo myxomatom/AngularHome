914ae060aa66da94271a466116aba8cd3ec94c34
Myxomatom <myxomatom@gmail.com>
2017-10-30 03:58:17 +0100

    Passage de bootstrap-ng à Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,90 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule }    from '@angular/http';
-import {FormsModule} from '@angular/forms';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatStepperModule,} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { GraphComponent } from './graph.component';
-import { NgbdDatepickerBasic } from './datepicker-basic';
+import { DatepickerOverview } from './datepicker-overview';
 import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
-    AppComponent,GraphComponent,NgbdDatepickerBasic
+    AppComponent,GraphComponent,DatepickerOverview,
   ],
+
   imports: [
     BrowserModule,
     ChartsModule,
+    FormsModule,
     HttpModule,
-    NgbModule.forRoot(),
-    FormsModule
+    BrowserAnimationsModule,
+    MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatStepperModule,
+
   ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/datepicker-overview.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic datepicker
+ */
+@Component({
+  selector: 'datepicker-overview',
+  templateUrl: 'datepicker-overview.html',
+  styleUrls: ['datepicker-overview.css'],
+})
+export class DatepickerOverview {}
